002ee418fa2ff347cb824e616dc456e008493e8d
sakshamp10 <96952593+sakshamp10@users.noreply.github.com>
2022-10-25 23:37:29 +0530

    Update assmnt1.c

--- assmnt1.c
+++ assmnt1.c
@@ -10,26 +10,85 @@ char** input_line(){
     getline(&inp,&size,stdin);
     int inp_length=10;
     char **words= malloc(inp_length*sizeof(char*));
-    char *word = strtok(inp, "\n\t\r ");
+    char *word = strtok(inp, " ");
     int i=0;
     while(word!=NULL){
         words[i]=word;
         if(++i>=inp_length){
-            inp_length++;
+            inp_length+=10;
             words=realloc(words,inp_length*sizeof(char*));
         }
-        word = strtok(NULL,"\n\t\r ");
+        word = strtok(NULL," ");
     }
     words[i]=NULL;
     return words;
 }
 
+void echo(char** input){
+
+    //left for later
+
+    for(int i=1;input[i]!=NULL;i++){
+        for(int j=0;input[i][j]!='\0';j++){
+            if(input[i][j]!='\\')
+                printf("%c",input[i][j]);
+            else{
+                if(input[i][j]=='\\'){
+                }
+            }
+        }
+        printf(" ");
+    }
+}
+
+void pwd(){
+    int size=0;
+    char *pwd=NULL;
+    pwd=getcwd(pwd,size);
+    char p=0;
+    while(pwd[p]!='\0'){
+        printf("%c",p);
+        p++;
+    }
+}
 
 int main(){
+
     while(1){
+        printf("~$ ");
         char **input=input_line();
-        for(int i=0;i<sizeof(input);i++){
-            printf("%s\n",*(input+i));
+        if(input[0]==NULL){
+            continue;
+        }
+        if(strcmp(input[0],"cd")==0){
+
+        }
+        else if(strcmp(input[0],"echo")){
+            echo(input);
+        }
+        else if(strcmp(input[0],"pwd")==0){
+            pwd();
+        }
+        else if(strcmp(input[0],"exit")==0){
+            break;
+        }
+        else if(strcmp(input[0],"ls")==0){
+
+        }
+        else if(strcmp(input[0],"cat")==0){
+
+        }
+        else if(strcmp(input[0],"date")==0){
+
+        }
+        else if(strcmp(input[0],"rm")==0){
+
+        }
+        else if(strcmp(input[0],"mkdir")==0){
+
+        }
+        else{
+            printf("%s: Command not found\n",input[0]);
         }
     }
     return 0;
