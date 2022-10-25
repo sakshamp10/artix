a0ee07e8202d9652adb36b4c4ffb829601fdd4bb
sakshamp10 <96952593+sakshamp10@users.noreply.github.com>
2022-10-26 00:53:34 +0530

    Update assmnt1.c

--- assmnt1.c
+++ assmnt1.c
@@ -24,75 +24,89 @@ char** input_line(){
     return words;
 }
 
-//void echo(char** input){
-//
-//    //left for later
-//
-//    for(int i=1;input[i]!=NULL;i++){
-//        for(int j=0;input[i][j]!='\0';j++){
-//            if(input[i][j]!='\\')
-//                printf("%c",input[i][j]);
-//            else{
-//                if(input[i][j]=='\\'){
-//                }
-//            }
-//        }
-//        printf(" ");
-//    }
-//}
-//
-//void pwd(){
-//    int size=0;
-//    char *pwd=NULL;
-//    pwd=getcwd(pwd,size);
-//    char p=0;
-//    while(pwd[p]!='\0'){
-//        printf("%c",p);
-//        p++;
-//    }
-//}
+void echo(char** input){
+
+    //left for later
+
+    for(int i=1;input[i]!=NULL;i++){
+        for(int j=0;input[i][j]!='\0';j++){
+            if(input[i][j]!='\\')
+                printf("%c",input[i][j]);
+            else{
+                if(input[i][j]=='\\'){
+                }
+            }
+        }
+        printf(" ");
+    }
+}
+
+void pwd(){
+    size_t size=0;
+    char *pwd=NULL;
+    pwd=getcwd(pwd,size);
+    char p=0;
+    while(pwd[p]!='\0'){
+        printf("%c",p);
+        p++;
+    }
+}
+
+void free2D(char** input){
+    int i=0;
+    while(input[i]!=NULL){
+        free(input[i]);
+        i++;
+    }
+    free(input);
+}
+
+char* delim(char* input){
+    input=strtok(input,"\n");
+    return input;
+}
 
 int main(){
-
     while(1){
         printf("~$ ");
         char **input=input_line();
-        for(int i=0;i<sizeof(input);i++){
-            printf("%s\n",input[i]);
-        }
+        char *inp=delim(input[0]);
         if(input[0]==NULL){
-            continue;
+
         }
         if(strcmp(input[0],"cd")==0){
 
         }
-        else if(strcmp(input[0],"echo")){
-//            echo(input);
+        else if(strcmp(inp,"exit")==0){
+            echo(input);
         }
-        else if(strcmp(input[0],"pwd")==0){
-//            pwd();
+        else if(strcmp(inp,"pwd")==0){
+            pwd();
         }
-        else if(strcmp(input[0],"exit")==0){
+        else if(strcmp(inp,"exit")==0){
             break;
         }
-        else if(strcmp(input[0],"ls")==0){
+        else if(strcmp(inp,"ls")==0){
 
         }
-        else if(strcmp(input[0],"cat")==0){
+        else if(strcmp(inp,"cat")==0){
 
         }
-        else if(strcmp(input[0],"date")==0){
+        else if(strcmp(inp,"date")==0){
 
         }
-        else if(strcmp(input[0],"rm")==0){
+        else if(strcmp(inp,"rm")==0){
 
         }
-        else if(strcmp(input[0],"mkdir")==0){
+        else if(strcmp(inp,"mkdir")==0){
 
         }
         else{
-            printf("%s: Command not found\n",input[0]);
+            printf("%s: Command not found\n",inp);
         }
+
+        free(inp);
+        free2D(input);
     }
     return 0;
 }
