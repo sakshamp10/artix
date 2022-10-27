f015f94329773b0d8e6fa2a160e6d4800d491497
Abhijeet Anand <108174938+AbhijeetAnand23@users.noreply.github.com>
2022-10-27 18:45:39 +0530

    Abhijeet 16

--- rm.c
+++ rm.c
@@ -14,5 +14,35 @@ int main(int argc, char* args[], char* envp[])
             exit(EXIT_FAILURE);
         }
     }
+    else if(strcmp(args[0], "-i") == 0)
+    {
+        char check;
+        printf("remove file %s? ", args[1]);
+        scanf("%c", &check);
+        if(check == 'y')
+        {
+            int status;
+            status = remove(args[1]);
+            if(status != 0)
+            {
+                perror("Couldn't remove the file");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    else if(strcmp(args[0], "-v") == 0)
+    {
+        int status;
+        status = remove(args[1]);
+        if(status != 0)
+        {
+            perror("Couldn't remove the file");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            printf("removed %s", args[1]);
+        }
+    }
     return 0;
 }
